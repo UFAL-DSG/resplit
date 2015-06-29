877ac96d5f7d7254149967d8539e4efa97b42878
Lukas Zilka <lukas@zilka.me>
2015-06-29 10:46:11 +0200

    Fixing.

--- recording_splitter.py
+++ recording_splitter.py
@@ -195,21 +195,21 @@ def _find_files_to_split(input_dir):
     for root, dirs, files in os.walk(input_dir):
         for file_name in files:
             if file_name.endswith('.pcm'):
-                to_process.append((file_name, os.path.relpath(root, start=input_dir)))
+                to_process.append((file_name, os.path.relpath(root, start=input_dir), root))
     return to_process
 
 
 def _split_files(rs, output_dir, to_process, pcm_sample_rate):
     logging.info('Processing files.')
-    for file_name, root in to_process:
+    for file_name, root, abs_root in to_process:
         file_out_dir = os.path.join(output_dir, root, file_name)
         print output_dir, file_name, root, file_out_dir
-        files = _split_2chan_pcm(rs, file_name, file_out_dir, pcm_sample_rate, root)
+        files = _split_2chan_pcm(rs, abs_root, file_name, file_out_dir, pcm_sample_rate, root)
         _create_session_xml(file_out_dir, files)
 
 
-def _split_2chan_pcm(rs, file_name, out_dir, sample_rate, root):
-    file_path = os.path.join(root, file_name)
+def _split_2chan_pcm(rs, abs_root, file_name, out_dir, sample_rate, root):
+    file_path = os.path.join(abs_root, file_name)
 
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
